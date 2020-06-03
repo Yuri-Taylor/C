9fc75f6599237a541cfb08ad98628b4679332489
Yuri-Taylor <yuri_taylor@163.com>
2020-06-03 10:54:33 +0800

    提交代码

---
+++ Project1/Project1/源.c
@@ -0,0 +1,73 @@
+//#include<stdio.h>
+
+//void Multiplication(int n)
+//{
+//	for (int i = 1; i <= n; i++)
+//	{
+//		for (int j = 1; j <= i; j++)
+//		{
+//			printf("%d * %d = %d\t", j, i, i*j);
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//int main()
+//{
+//	int line;
+//	line = 6;
+//	Multiplication(line);
+//	return 0;
+//}
+
+//void Swap(int*a, int*b)
+//{
+//	int *tmp = 0;
+//	tmp = *a;
+//	*a = *b;
+//	*b = tmp;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	Swap(&a,&b);
+//	printf("a = %d, b = %d\n",a,b);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//void init(int arr[])
+//{
+//	scanf_s("%d", arr);
+//}
+//
+//void empty(int arr[20])
+//{
+//
+//}
+//
+//void reverse(int arr[20])
+//{
+//
+//}
+//
+//int main()
+//{
+//	int arr[20];
+//	init(arr);
+//	printf("%d \n", arr);
+//	//empty(arr);
+//	//printf("%d \n", arr);
+//	//reverse(arr);
+//	//printf("%d \n", arr);
+//	return 0;
+//}
+
+#include<stdio.h>
+int main()
+{
+
+	return 0;
+}
