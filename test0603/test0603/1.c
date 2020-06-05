e67e66bea36f5cc32897982875147d60c1c389e6
Yuri-Taylor <yuri_taylor@163.com>
2020-06-05 17:21:28 +0800

    提交代码

--- test0603/test0603/1.c
+++ test0603/test0603/1.c
@@ -1,21 +1,60 @@
 //实现一个函数，打印乘法口诀表，口诀表的行数和列数自己指定
 //输入9，输出9*9口诀表，输出12，输出12*12的乘法口诀表
+//#include<stdio.h>
+//void Multiplication(int n)
+//{
+//	for (int i = 1; i <= n; i++)
+//	{
+//		for (int j = 1; j <= i; j++)
+//		{
+//			printf("%d * %d = %d\t", j, i, i*j);
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int line;
+//	line = 12;
+//	Multiplication(line);
+//	return 0;
+//}
+;
+
+
+//编写一个程序，可以直接接收键盘字符，如果是小写字符就输出对应的大写字符，
+//如果接收的是大写字符，就输出对应的小写字符，如果是数字不输出。
+//#include<stdio.h>
+//int main()
+//{
+//	char ch = 0;
+//	while ((ch=getchar()) != EOF)
+//	{
+//		if (ch >= 'A'&&ch <= 'Z')
+//		{
+//			putchar(ch + 32);
+//		}
+//		else if (ch >= 'a'&&ch <= 'z')
+//		{
+//			putchar(ch - 32);
+//		}
+//		else if (ch >= '0' && ch <= '9')
+//		{
+//			;
+//		}
+//		else
+//		{
+//			putchar(ch);
+//		}
+//	}
+//	return 0;
+//}
+
+
+
+
 #include<stdio.h>
-void Multiplication(int n)
-{
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = 1; j <= i; j++)
-		{
-			printf("%d * %d = %d\t", j, i, i*j);
-		}
-		printf("\n");
-	}
-}
 int main()
 {
-	int line;
-	line = 12;
-	Multiplication(line);
 	return 0;
 }
