5a481bac97443e3ad431821481685da0461d1f32
Yuri-Taylor <yuri_taylor@163.com>
2020-08-14 18:00:12 +0800

    提交代码

--- test0803二叉树/test0803二叉树/test.c
+++ test0803二叉树/test0803二叉树/test.c
@@ -1,9 +1,38 @@
 #include"Heap.h"
 #include"BinaryTree.h"
 
-
+BTNode* Rebulid(char* a,int* pi)
+{
+	if (a[*pi] == '#')
+		return NULL;
+	else
+	{
+		BTNode* root = (BTNode*)malloc(sizeof(BTNode));
+		root->_data = a[*pi];
+		++(*pi);
+		root->_left = Rebulid(a,pi);
+		++(*pi);
+		root->_right = Rebulid(a, pi);
+		return root;
+	}
+}
+void Inorder(BTNode* root)
+{
+	if (root == NULL)
+		return;
+	Inorder(root->_left);
+	printf("%c ", root->_data);
+	Inorder(root->_right);
+}
 int main()
 {
-	//TestTopK();
+	char a[100];
+	scanf("%s", a);
+	//根据先序遍历重构二叉树
+	int i = 0;
+	BTNode* root = NULL;
+	root = Rebulid(a, i);
+	//中序输出
+	Inorder(root);
 	return 0;
 }
