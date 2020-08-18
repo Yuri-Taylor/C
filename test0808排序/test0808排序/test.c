678878a42d84469f519c2c3cd700173662e51111
Yuri-Taylor <yuri_taylor@163.com>
2020-08-18 21:17:36 +0800

    提交代码

--- test0808排序/test0808排序/test.c
+++ test0808排序/test0808排序/test.c
@@ -1,30 +1,6 @@
 #include<stdio.h>
 
-void bubbleSort(int *arr, int n)
-
-{
-
-	int m, i, j;
-
-	for (i = 0; i < n - 1; i++)
-
-	/*	for (j = 0; j < n - 1 - i; j++)
-
-			if (arr[j] > arr[j + 1])
-
-			{
-
-				m = arr[j];
-
-				arr[j] = arr[j + 1];
-
-				arr[j + 1] = m;
-
-			}
-
-}
-
 int main()
 {
 	return 0;
-}*/
+}
